a961d6c4cbc082b8a9cd463dcc6806dbacc41608
kiran <kiranreddy@kiran-Reddy-2.local>
2020-11-01 10:42:19 +0530

    Pushing the folder to git

---
+++ flutter-login-screen-firebase-auth-facebook-login/lib/constants.dart
@@ -0,0 +1,8 @@
+library constants;
+
+const FINISHED_ON_BOARDING = 'finishedOnBoarding';
+const COLOR_ACCENT = 0xFF9b979c;
+const COLOR_PRIMARY_DARK = 0xFF474648;
+const COLOR_PRIMARY = 0xFF848486;
+const FACEBOOK_BUTTON_COLOR = 0xFF415893;
+const USERS = 'users';
